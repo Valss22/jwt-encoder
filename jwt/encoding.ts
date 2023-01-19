b7a21c760135ab7399e31a3de8d96ba049bc66fa
vladislavshokorov <valsshokorov@gmail.com>
2023-01-19 15:53:41 +0600

    JWTEncoder class added

--- jwt/encoding.ts
+++ jwt/encoding.ts
@@ -3,27 +3,29 @@ import { Payload, Header, DefaultHeader } from "./types";
 
 const crypto = require("crypto");
 
-export const encodeHeader = (header: Header = DefaultHeader): string => {
-  const headerJSON = JSON.stringify(header);
-  return Buffer.from(headerJSON).toString("base64");
-};
+export class JWTEncoder {
+  encodeHeader(header: Header = DefaultHeader): string {
+    const headerJSON = JSON.stringify(header);
+    return Buffer.from(headerJSON).toString("base64");
+  }
 
-export const encodePayload = (payload: Payload): string => {
-  const payloadJSON = JSON.stringify(payload);
-  return Buffer.from(payloadJSON).toString("base64");
-};
+  encodePayload(payload: Payload): string {
+    const payloadJSON = JSON.stringify(payload);
+    return Buffer.from(payloadJSON).toString("base64");
+  }
 
-export const getUnsignedToken = (header = DefaultHeader, payload): string => {
-  return encodeHeader(header) + "." + encodePayload(payload);
-};
+  protected getUnsignedToken(header = DefaultHeader, payload): string {
+    return this.encodeHeader(header) + "." + this.encodePayload(payload);
+  }
 
-export const getSignature = (
-  header: Header = DefaultHeader,
-  payload: Payload,
-  secretKey: string
-) => {
-  return crypto
-    .createHmac("SHA256", secretKey)
-    .update(getUnsignedToken(header, payload))
-    .digest("base64");
-};
+  protected getSignature(
+    header: Header = DefaultHeader,
+    payload: Payload,
+    secretKey: string
+  ) {
+    return crypto
+      .createHmac("SHA256", secretKey)
+      .update(this.getUnsignedToken(header, payload))
+      .digest("base64");
+  }
+}
